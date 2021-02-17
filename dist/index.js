e6ad766520bc9b6f963d6eb34e881e20084bcfa6
Kitty Giraudel <hugo.giraudel@gmail.com>
2021-02-17 12:07:54 +0100

    Add first version of library

---
+++ dist/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var useDisclosure = function useDisclosure(props) {
+  var _React$useState = _react["default"].useState(typeof props.isExpanded !== 'undefined' ? props.isExpanded : true),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      isExpanded = _React$useState2[0],
+      setIsExpanded = _React$useState2[1];
+
+  _react["default"].useEffect(function () {
+    return typeof props.isExpanded !== 'undefined' && setIsExpanded(props.isExpanded);
+  }, [props.isExpanded]);
+
+  var toggle = _react["default"].useCallback(function () {
+    return setIsExpanded(function (v) {
+      return !v;
+    });
+  }, []);
+
+  var toggleProps = {
+    id: props.id + '-toggle',
+    onClick: toggle,
+    type: 'button',
+    'aria-expanded': isExpanded,
+    'aria-controls': props.id + '-content'
+  };
+  var contentProps = {
+    id: props.id + '-content',
+    'aria-hidden': !isExpanded,
+    'aria-labelledby': props.id + '-toggle'
+  };
+  return {
+    toggleProps: toggleProps,
+    contentProps: contentProps,
+    isExpanded: isExpanded,
+    toggle: toggle
+  };
+};
+
+var _default = useDisclosure;
+exports["default"] = _default;
---
+++ index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const useDisclosure = (props) => {
+  const [isExpanded, setIsExpanded] = React.useState(
+    typeof props.isExpanded !== 'undefined' ? props.isExpanded : true
+  )
+
+  React.useEffect(
+    () =>
+      typeof props.isExpanded !== 'undefined' &&
+      setIsExpanded(props.isExpanded),
+    [props.isExpanded]
+  )
+
+  const toggle = React.useCallback(() => setIsExpanded((v) => !v), [])
+
+  const toggleProps = {
+    id: props.id + '-toggle',
+    onClick: toggle,
+    type: 'button',
+    'aria-expanded': isExpanded,
+    'aria-controls': props.id + '-content',
+  }
+  const contentProps = {
+    id: props.id + '-content',
+    'aria-hidden': !isExpanded,
+    'aria-labelledby': props.id + '-toggle',
+  }
+
+  return { toggleProps, contentProps, isExpanded, toggle }
+}
+
+export default useDisclosure
